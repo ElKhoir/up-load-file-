f92578f02518b012c278e07dc29e61c9396a0b53
ElKhoir <158816123+ElKhoir@users.noreply.github.com>
2025-08-13 10:27:44 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,9 +1,6 @@
-
+// api/index.js
 import serverless from 'serverless-http';
 import app from '../server-app.js';
 
-export const config = {
-  maxDuration: 10
-};
-
+export const config = { maxDuration: 10, regions: ['sin1'] };
 export default serverless(app);
